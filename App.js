e61ee458df22b403c42ec87ab23feb0b27078c6e
TarekArar <gt_arar@esi.dz>
2019-08-18 18:14:55 +0100

    Second commit

--- App.js
+++ App.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {MapView, Marker} from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}
-      region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}>
-        
-      </MapView>
-    </View>
+     <MapView
+       showsUserLocation={true}  
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left : 0,
-    right : 0,
-    bottom : 0,
-    justifyContent : "felx-end",
-    alignItems : "center"
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
-  }
-  
-});
+    ...StyleSheet.absoluteFillObject,
+    flex: 1
+  },
+ });
+ 
